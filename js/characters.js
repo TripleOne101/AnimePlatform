4895087e50f48761f0d4c7812bffcc74832f37e5
Ziad-Abazed <ziad.w.abazeed@gmail.com>
2023-06-12 12:33:07 +0000

    edit the form in characters page store the form inputs in local storge

--- js/characters.js
+++ js/characters.js
@@ -5,19 +5,22 @@ const audio = new Audio(soundUrl);
 soundIcon.addEventListener("click", () => {
   audio.play();
 });
-
+let UserInputs=[]
 function Input ( yourname, chname,quote,image) {
   this.yourname = yourname;
   this.chname = chname;
   this.quote = quote;
   this.image = image;
+  UserInputs.push(this);
 
 }
 
-Input.prototype.render = function () {
+ function render () {
 
     let forminf = document.getElementsByClassName("quotes")[0];
-
+    forminf.innerHTML=``;
+ 
+    for (let i = 0; i < UserInputs.length; i++) {
     let information = document.createElement("div");
     information.classList.add("cards"); 
     forminf.appendChild(information);
@@ -25,18 +28,18 @@ Input.prototype.render = function () {
 
 
     let yourname = document.createElement("p");
-    yourname.textContent = this.yourname;
+    yourname.textContent = UserInputs[i].yourname;
     yourname.classList.add("input-field");
     yourname.style.color = "white";
     information.appendChild(yourname);
     
     let chname = document.createElement("p")
-    chname.textContent = this.chname;
+    chname.textContent = UserInputs[i].chname;
     chname.style.color = "white";
     information.appendChild(chname);
     
     let quote = document.createElement("h3")
-    quote.textContent = this.quote;
+    quote.textContent = UserInputs[i].quote;
     quote.style.color = "white";
     information.appendChild(quote);
 
@@ -44,13 +47,16 @@ Input.prototype.render = function () {
     // information.appendChild(imageContainer);
 
     let image = document.createElement("img")
-    image.setAttribute('src', this.image);
+    image.setAttribute('src', UserInputs[i].image);
     image.style.width = "200px"; 
     image.style.height = "200px"; 
     information.appendChild(image);
  
     
-}
+}}
+let saveValues = document.getElementById("character-form");
+saveValues.addEventListener('submit', handler);  
+
 
 function handler(e){
     e.preventDefault();
@@ -68,13 +74,58 @@ function handler(e){
     }
   
    
-    let forminf = new Input( yourname,chname,quote,image);
-    forminf.render();
+ new Input( yourname,chname,quote,image);
     
-    
-    console.log(yourname,chname,quote);
+    storeIntoLocalStorage();
+  
+    render();
+
+ 
 };
 
-let saveValues = document.getElementById("character-form");
-saveValues.addEventListener('submit', handler);
 
+function storeIntoLocalStorage() {
+ 
+  // convert JS to JSON 
+  let jsonObjArray = JSON.stringify(UserInputs);
+  window.localStorage.setItem("characters", jsonObjArray);
+}
+
+
+function readFromLocalStorage() {
+  // read the json Array From LS 
+  let jsonArray = window.localStorage.getItem("characters");
+  // convert the JSON to JS 
+  let objArray = JSON.parse(jsonArray);
+  console.log("Array after read From LS before the re-intantiation => ", objArray);
+
+  if (objArray == null) {
+      console.log("The LS is Empty");
+  } else {
+      
+
+      for(let i = 0 ; i< objArray.length; i++){
+      new Input (objArray[i].yourname , objArray[i].chname , objArray[i].quote , objArray[i].image);
+
+      }
+     
+
+  }
+
+
+}
+
+
+
+readFromLocalStorage();
+
+var something = (function() {
+  var executed = false;
+  return function() {
+      if (!executed) {
+          executed = true;
+          render();
+      }
+  };
+})();
+something()
