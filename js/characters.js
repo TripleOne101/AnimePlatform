16997903b46dde4bfcb71eec74e685dbcf95d514
rulaSalem99 <rula.qudah.1@gmail.com>
2023-06-13 18:17:21 +0000

    header and footer

--- js/characters.js
+++ js/characters.js
@@ -1,4 +1,54 @@
 'use strict';
+
+let dropDown=document.getElementById("subMenu");
+let currentUserName=localStorage.getItem("userEmail");
+console.log(currentUserName);
+if (currentUserName==null){
+    dropDown.innerHTML=` <div class="sub-menu">
+    <div class="user-info">
+        <img
+            src="https://th.bing.com/th/id/R.bc8abf4164cb9ae12872dd77956f3753?rik=gno2jmkaAlh1yw&riu=http%3a%2f%2fbonniesomerville.nz%2fwp-content%2fuploads%2f2015%2f08%2fprofile-icon.png&ehk=RFa%2b%2b54P%2b29B%2fafZdZZ7xGNy16dQxFLBv8Q7Sh9xyVY%3d&risl=&pid=ImgRaw&r=0">
+        <h2 id="guest">guest</h2>
+    </div>
+    <hr>
+
+    <a href="profile.html" class="sub-menu-link"><i class="fas fa-user"></i>
+        <p>Profile</p>
+        <span>></span>
+    </a>
+
+    <a href="./sign-in.html" class="sub-menu-link"><i class="fa-solid fa-right-to-bracket" style="color: #ffffff;"></i>
+        <p>Login</p>
+        <span>></span>
+    </a>
+
+</div>`
+} else {
+    dropDown.innerHTML=` <div class="sub-menu">
+    <div class="user-info">
+        <img
+            src="https://th.bing.com/th/id/R.bc8abf4164cb9ae12872dd77956f3753?rik=gno2jmkaAlh1yw&riu=http%3a%2f%2fbonniesomerville.nz%2fwp-content%2fuploads%2f2015%2f08%2fprofile-icon.png&ehk=RFa%2b%2b54P%2b29B%2fafZdZZ7xGNy16dQxFLBv8Q7Sh9xyVY%3d&risl=&pid=ImgRaw&r=0">
+        <h2 id="guest">${currentUserName}</h2>
+    </div>
+    <hr>
+
+    <a href="profile.html" class="sub-menu-link"><i class="fas fa-user"></i>
+        <p>Profile</p>
+        <span>></span>
+    </a>
+
+    <a href="./index.html" class="sub-menu-link" onclick="clearUser()"><i class="fa-solid fa-right-to-bracket" style="color: #ffffff;"></i>
+        <p>Logout</p> 
+        <span>></span>
+    </a>
+
+</div>` 
+}
+
+function clearUser(){
+    localStorage.removeItem('userEmail');
+}
+
 const soundIcon = document.getElementById("sound-icon");
 const soundUrl = "js/sounds/mashup spacetoon songs -.mp3";
 const audio = new Audio(soundUrl);
@@ -133,3 +183,8 @@ var something = (function() {
   };
 })();
 something()
+
+
+let userName=window.localStorage.getItem('userEmail')?? "Guest1001";
+let userNamediv=document.getElementById("nameUser");
+userNamediv.innerHTML=userName;
